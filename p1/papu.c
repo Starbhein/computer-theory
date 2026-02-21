7fe91fc2c3ffbff7b0d9b495384811c983812b87
Starbhein <starbheingt01@gmail.com>
2026-02-21 17:11:59 -0600

    Mejora de funcion de potencia y aplicando memcpy pero sin usar recursividad, evitando cadenas que crecen como suma algebraica, esto se convierte en un O(n)

--- p1/papu.c
+++ p1/papu.c
@@ -14,9 +14,23 @@ int valCad(char *a, char *b){
   return (strlen(a) < 50 || strlen(b) < 50);
 
 }
+char *reverseC(char *a){
+  char *x = (char*)malloc((strlen(a)+1) *sizeof(char));
+  memcpy(x,a,strlen(a));
+  char temp;
+  for(char *k = x, *z = x + strlen(x)-1; k < z; k++,z--){
+    temp = *k;
+    *k = *z;
+    *z = temp;
+  }
+  return  x;
+}
 char *concat(char *a, char *b){
+  
   int l1 = strlen(a);
   int l2 = strlen(b);
+  if(l1 == 0 && l2== 0) return "λ";
+
   char *z = (char*) malloc((l1 + l2) * sizeof(char));
   memcpy(z, a,l1);
   memcpy(z+l1,b,l2);
@@ -26,11 +40,13 @@ char *concat(char *a, char *b){
 }
 
 char *poti(char *a,int pt){
-  char *z = NULL;
   if(pt == 0){
-    return NULL;
+    return "λ";
   }
-  return NULL; 
+  if(pt < 0){
+    return poti(reverseC(a),pt * (-1));
+  }
+  return concat(a,poti(a,pt-1)); 
 }
 void pref(char *k){
   char *a = k;
@@ -40,6 +56,28 @@ void pref(char *k){
   }
   printf("%s\n", "λ");
 }
+
+char *potiOpt(char *a, int pt){
+  if(pt == 0) return "λ";
+  int sz = strlen(a);
+  char *z;
+  if(pt < 0){
+    z = reverseC(a);
+    pt *= -1;
+  }else{
+    z = a;
+  }
+  char *x = (char*)malloc(sizeof(char) * ((sz * pt) + 1));
+  for(int i = 0; i < pt; i++){
+    memcpy(x + (sz *i), z, sz );
+  }
+  x[pt * sz]= '\0';
+  if(z!=a) free(z);
+
+  return x;
+
+}
+
 
 void sufix(char *k){
   char *a = k;
@@ -58,14 +96,15 @@ int main(){
   //for(int i =0; k[i] != '\0'; i++){
     //u[6] = u[6] >> 8;
   //}
-
+  printf("reversed string %s \n", reverseC(u));
   //printf("%s", u);
   //char *p = concat(k,z);
   //printf("%d", strlen(""));
   //printf("%d", valCad(z,z));
-  pref(u);
+  printf("concat of u to n poti: %s ", potiOpt(u,-10));
+ // pref(u);
 
-  printf("u: %s\n",u);
-  sufix(u);
+  //printf("u: %s\n",u);
+  //sufix(u);
 
 }
